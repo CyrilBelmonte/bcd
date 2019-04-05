eafd9839a23577f35b12aa1335eb5be25e5b32d0
EternityGit <loic.tramis@gmail.com>
2019-04-05 22:03:23 +0200

    :construction: BD relationnel

--- src/main/java/com/ucp/scrapper/database/ddl.sql
+++ src/main/java/com/ucp/scrapper/database/ddl.sql
@@ -1,75 +1,43 @@
 DROP DATABASE IF EXISTS BigCookingData;
 
-DROP TABLE IF EXISTS User;
+CREATE DATABASE BigCookingData;
+
+DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Shares;
 DROP TABLE IF EXISTS Likes;
 DROP TABLE IF EXISTS Comments;
-DROP TABLE IF EXISTS Ingredients;
-DROP TABLE IF EXISTS Steps;
-DROP TABLE IF EXISTS Categories;
-DROP TABLE IF EXISTS Recipes;
+DROP TABLE IF EXISTS Ingredient;
+DROP TABLE IF EXISTS Step;
+DROP TABLE IF EXISTS Category;
+DROP TABLE IF EXISTS Recipe;
 
-CREATE DATABASE BigCookingData;
-
-CREATE TABLE User (
-    id_user INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR2(50),
-    surname VARCHAR2(50),
-    likes INT,
-    shares INT,
-    creation_time DATE,
-    PRIMARY KEY(id_user)
-);
-CREATE TABLE Share (
-    id_share INT NOT NULL AUTO_INCREMENT,
-    id_recipe INT,
-    id_user INT,
-    PRIMARY KEY(id_share),    
-    FOREIGN KEY (id_user) REFERENCES User(id_user),
-    FOREIGN KEY (id_recipe) REFERENCES Recipe(id_recipes)
-);
-CREATE TABLE Like (
-    id_like INT NOT NULL AUTO_INCREMENT,
-    id_user INT,
-    id_recipe INT,
-    PRIMARY KEY(id_like),
-    FOREIGN KEY (id_user) REFERENCES User(id_user),
-    FOREIGN KEY (id_recipe) REFERENCES Recipe(id_recipe)
-);
-CREATE TABLE Comment (
+CREATE TABLE Comments (
     id_comment INT NOT NULL AUTO_INCREMENT,
-    text VARCHAR2(500),
+    text VARCHAR(500),
     date DATE,
-    user VARCHAR2(50),
-    mark VARCHAR2(20),
+    user VARCHAR(50),
+    mark VARCHAR(20),
     id_recipe INT,
-    PRIMARY KEY(id_comment),
+    PRIMARY KEY(id_comment)
 );
 CREATE TABLE Ingredient (
     id_ingredient INT NOT NULL AUTO_INCREMENT,
     quantity INT,
-    name VARCHAR2(50),
+    name VARCHAR(50),
     picture BLOB,
-    id_recipe INT
-    PRIMARY KEY(id_ingredient),
-    FOREIGN KEY (id_recipe) REFERENCES Recipe(id_recipe)
+    id_recipe INT,
+    PRIMARY KEY(id_ingredient)
 );
 CREATE TABLE Step (
     id_step INT NOT NULL AUTO_INCREMENT,
     step_Number INT,
-    instruction VARCHAR2(500),
+    instruction VARCHAR(500),
     PRIMARY KEY(id_step)
 );
-CREATE TABLE Category (
-    id_category INT NOT NULL AUTO_INCREMENT,
-    description VARCHAR2(200),
-    id_recipe INT,
-    PRIMARY KEY(id_category),
-    FOREIGN KEY (id_recipe) REFERENCES Recipe(id_recipe)    
-);
+
 CREATE TABLE Recipe (
     id_recipe INT NOT NULL AUTO_INCREMENT,
-    mark VARCHAR2(50),
+    mark VARCHAR(50),
     id_ingredient INT,
     id_comment INT,
     id_step INT,
@@ -78,6 +46,40 @@ CREATE TABLE Recipe (
     recipeTime INT,
     PRIMARY KEY (id_recipe),
     FOREIGN KEY (id_ingredient) REFERENCES Ingredient(id_ingredient),
-    FOREIGN KEY (id_comment) REFERENCES Comment(id_comment),
+    FOREIGN KEY (id_comment) REFERENCES Comments(id_comment),
     FOREIGN KEY (id_step) REFERENCES Step(id_step)
+);
+
+CREATE TABLE Users (
+    id_user INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+    surname VARCHAR(50),
+    likes INT,
+    shares INT,
+    creation_time DATE,
+    PRIMARY KEY(id_user)
+);
+CREATE TABLE Shares (
+    id_share INT NOT NULL AUTO_INCREMENT,
+    id_recipe INT,
+    id_user INT,
+    PRIMARY KEY(id_share),
+    FOREIGN KEY (id_user) REFERENCES Users(id_user),
+    FOREIGN KEY (id_recipe) REFERENCES Recipe(id_recipe)
+);
+CREATE TABLE Likes (
+    id_like INT NOT NULL AUTO_INCREMENT,
+    id_user INT,
+    id_recipe INT,
+    PRIMARY KEY(id_like),
+    FOREIGN KEY (id_user) REFERENCES Users(id_user),
+    FOREIGN KEY (id_recipe) REFERENCES Recipe(id_recipe)
+);
+
+CREATE TABLE Category (
+    id_category INT NOT NULL AUTO_INCREMENT,
+    description VARCHAR(200),
+    id_recipe INT,
+    PRIMARY KEY(id_category),
+    FOREIGN KEY (id_recipe) REFERENCES Recipe(id_recipe)
 );
