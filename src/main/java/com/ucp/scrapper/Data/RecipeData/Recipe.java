08e761eb4d9931864e2343aab3a2ea62b65bc38b
EternityGit <loic.tramis@gmail.com>
2019-04-05 22:01:04 +0200

    Merge branch 'scrapper' of C:\Users\Entropy\IdeaProjects\bcd with conflicts.

--- src/main/java/com/ucp/scrapper/Data/RecipeData/Recipe.java
+++ src/main/java/com/ucp/scrapper/Data/RecipeData/Recipe.java
@@ -1,59 +1,32 @@
 package com.ucp.scrapper.Data.RecipeData;
 
 
+import com.ucp.scrapper.data.recipedata.Ingredient;
+import com.ucp.scrapper.data.recipedata.Step;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+
 public class Recipe {
 
     private int recipeTime;
     private int cookingTime;
     private int preparationTime;
     private int numberPersons;
-    private int mark;
+    private float mark;
 
     private String economicLevel;
     private String difficultyLevel;
     private String title;
     private String picture;
 
-    private List<Ingredient> ingredients;
-    private List<WebComment> webComments;
-    private List<Step> steps;
+    private ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
+    private ArrayList<Step> steps = new ArrayList<Step>();
 
-    public static class RecipeBuilder {
-        public RecipeBuilder setIngredientBuilder(Ingredient... ingredients) {
-            this.ingredients = new ArrayList<Ingredient>();
-            for (Ingredient ingredient : ingredients) {
-                this.ingredients.add(ingredient);
-            }
-            return this;
-        }
-
-        public RecipeBuilder setCommentBuilder(WebComment... webComments) {
-            this.webComments = new ArrayList<WebComment>();
-            for (WebComment webComment : webComments) {
-                this.webComments.add(webComment);
-            }
-            return this;
-        }
-
-        public RecipeBuilder setStepBuilder(Step... steps) {
-            this.steps = new ArrayList<Step>();
-            for (Step step : steps) {
-                this.steps.add(step);
-            }
-            return this;
-        }
-
-    }
 }
--- src/main/java/com/ucp/scrapper/database/ddl.sql
+++ src/main/java/com/ucp/scrapper/database/ddl.sql
@@ -1,4 +1,6 @@
-DROP TABLE IF EXISTS Users;
+DROP DATABASE IF EXISTS BigCookingData;
+
+DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Shares;
 DROP TABLE IF EXISTS Likes;
 DROP TABLE IF EXISTS Comments;
@@ -7,65 +9,67 @@ DROP TABLE IF EXISTS Steps;
 DROP TABLE IF EXISTS Categories;
 DROP TABLE IF EXISTS Recipes;
 
-CREATE TABLE Users (
+CREATE DATABASE BigCookingData;
+
+CREATE TABLE User (
     id_user INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50),
-    surname VARCHAR(50),
+    name VARCHAR2(50),
+    surname VARCHAR2(50),
     likes INT,
     shares INT,
     creation_time DATE,
     PRIMARY KEY(id_user)
 );
-CREATE TABLE Shares (
+CREATE TABLE Share (
     id_share INT NOT NULL AUTO_INCREMENT,
     id_recipe INT,
     id_user INT,
     PRIMARY KEY(id_share),    
-    FOREIGN KEY (id_user) REFERENCES Users (id_user),
-    FOREIGN KEY (id_recipe) REFERENCES Recipes (id_recipes)
+    FOREIGN KEY (id_user) REFERENCES User(id_user),
+    FOREIGN KEY (id_recipe) REFERENCES Recipe(id_recipes)
 );
-CREATE TABLE Likes (
+CREATE TABLE Like (
     id_like INT NOT NULL AUTO_INCREMENT,
     id_user INT,
     id_recipe INT,
     PRIMARY KEY(id_like),
-    FOREIGN KEY (id_user) REFERENCES Users (id_user),
-    FOREIGN KEY (id_recipe) REFERENCES Recipes (id_recipe)
+    FOREIGN KEY (id_user) REFERENCES User(id_user),
+    FOREIGN KEY (id_recipe) REFERENCES Recipe(id_recipe)
 );
-CREATE TABLE Comments (
+CREATE TABLE Comment (
     id_comment INT NOT NULL AUTO_INCREMENT,
-    text VARCHAR(500),
+    text VARCHAR2(500),
     date DATE,
-    user VARCHAR(50),
-    mark VARCHAR(20),
+    user VARCHAR2(50),
+    mark VARCHAR2(20),
     id_recipe INT,
     PRIMARY KEY(id_comment),
 );
-CREATE TABLE Ingredients (
+CREATE TABLE Ingredient (
     id_ingredient INT NOT NULL AUTO_INCREMENT,
     quantity INT,
-    name VARCHAR(50),
+    name VARCHAR2(50),
     picture BLOB,
-    id_recipe INT,
+    id_recipe INT
     PRIMARY KEY(id_ingredient),
-    FOREIGN KEY (id_recipe) REFERENCES Recipes (id_recipe)
+    FOREIGN KEY (id_recipe) REFERENCES Recipe(id_recipe)
 );
-CREATE TABLE Steps (
+CREATE TABLE Step (
     id_step INT NOT NULL AUTO_INCREMENT,
     step_Number INT,
-    instruction VARCHAR(500),
+    instruction VARCHAR2(500),
     PRIMARY KEY(id_step)
 );
-CREATE TABLE Categories (
+CREATE TABLE Category (
     id_category INT NOT NULL AUTO_INCREMENT,
-    description VARCHAR(200),
+    description VARCHAR2(200),
     id_recipe INT,
     PRIMARY KEY(id_category),
-    FOREIGN KEY (id_recipe) REFERENCES Recipes (id_recipe)
+    FOREIGN KEY (id_recipe) REFERENCES Recipe(id_recipe)    
 );
-CREATE TABLE Recipes (
+CREATE TABLE Recipe (
     id_recipe INT NOT NULL AUTO_INCREMENT,
-    mark VARCHAR(50),
+    mark VARCHAR2(50),
     id_ingredient INT,
     id_comment INT,
     id_step INT,
@@ -73,7 +77,7 @@ CREATE TABLE Recipes (
     preparationTime INT,
     recipeTime INT,
     PRIMARY KEY (id_recipe),
-    FOREIGN KEY (id_ingredient) REFERENCES Ingredients (id_ingredient),
-    FOREIGN KEY (id_comment) REFERENCES Comments (id_comment),
-    FOREIGN KEY (id_step) REFERENCES Steps (id_step)
+    FOREIGN KEY (id_ingredient) REFERENCES Ingredient(id_ingredient),
+    FOREIGN KEY (id_comment) REFERENCES Comment(id_comment),
+    FOREIGN KEY (id_step) REFERENCES Step(id_step)
 );
