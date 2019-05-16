2d9e12b06908614dee4bb43cd772bf88167c2c5c
CyrilBelmonte <cyril.belmonte@hotmail.fr>
2019-05-16 22:13:40 +0200

    adding conf and example file

--- src/main/java/com/ucp/xml/exist/ExistDB.java
+++
@@ -1,5 +0,0 @@
-package com.ucp.xml.exist;
-
-
-public class ExistDB {
-}
---
+++ src/main/java/com/ucp/xml/exist/conf/InitExistDB.java
@@ -0,0 +1,28 @@
+package com.ucp.xml.exist.conf;
+
+public class InitExistDB {
+
+    private String collectionPath = "bcd";
+    private String URIDB = "xmldb:exist://localhost:8080/exist/xmlrpc";
+
+    private  String fileCategoryPath,fileProfilePath,fileUserPath;
+
+    public InitExistDB(String fileCategoryPath,String fileProfilePath,String fileUserPath){
+        this.fileCategoryPath=fileCategoryPath;
+        this.fileProfilePath=fileProfilePath;
+        this.fileUserPath=fileUserPath;
+    }
+
+    public void init() throws Exception{
+
+        CollectionDB collectionDB = new CollectionDB();
+
+        collectionDB.addCollectionDB(collectionPath,URIDB);
+
+        collectionDB.addFileDB(collectionPath,fileCategoryPath,URIDB);
+        collectionDB.addFileDB(collectionPath,fileProfilePath,URIDB);
+        collectionDB.addFileDB(collectionPath,fileUserPath,URIDB);
+    }
+
+
+}
