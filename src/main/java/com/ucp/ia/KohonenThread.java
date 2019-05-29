50db380be61eb2fda68e1576cf6c54d3e084d56d
Archambault Vincent <vincent.archambault95@gmail.com>
2019-05-29 13:30:45 +0200

    uptade thread ia

--- src/main/java/com/ucp/ia/KohonenThread.java
+++ src/main/java/com/ucp/ia/KohonenThread.java
@@ -6,19 +6,25 @@ import java.util.LinkedList;
 
 public class KohonenThread extends  Thread {
 
-
-
+    LinkedList<String> TitleList;
+    private LinkedList<String> ingredients;
+    private LinkedList<Recipe> recipes ;
     private  Kohonen kohonen;
     private String type;
     public KohonenThread(LinkedList<String> ingredients, LinkedList<Recipe> recipes, LinkedList<String> TitleList, String type) {
-        kohonen=new Kohonen(ingredients,recipes,TitleList);
+        this.TitleList=TitleList;
+        this.recipes=recipes;
+        this.ingredients=ingredients;
         this.type=type;
     }
 
     public void run(){
-       System.out.println("Clustering kohonen "+type);
+        System.out.println("Init kohonen"+type+"start");
+        kohonen=new Kohonen(ingredients,recipes,TitleList);
+        System.out.println(" Init kohonen "+type+"end");
+       System.out.println("Clustering "+type+"start");
        kohonen.Clustering();
-        System.out.println("Clustering end "+type);
+        System.out.println("Clustering "+type+"end");
     }
 
     public Kohonen getKohonen() {
