b6ed379a57043e63202a171dbcd9d91cd524f307
CyrilBelmonte <cyril.belmonte@hotmail.fr>
2019-05-29 14:51:37 +0200

    correcting

--- src/main/java/com/ucp/xml/parse_xml/user_xml/dao/test/TestSimpleUser.java
+++ src/main/java/com/ucp/xml/parse_xml/user_xml/dao/test/TestSimpleUser.java
@@ -1,10 +1,13 @@
 package com.ucp.xml.parse_xml.user_xml.dao.test;
 
+import com.ucp.xml.exist.query.QueryCategory;
 import com.ucp.xml.exist.query.QuerySimpleUser;
 
 public class TestSimpleUser {
     public static void main(String[] args) {
         QuerySimpleUser querySimpleUser = new QuerySimpleUser();
+        QueryCategory queryCategory = new QueryCategory();
         System.out.println(querySimpleUser.getFirstCategory(1,"starter"));
+        System.out.println(queryCategory.findCatByRecipe(22));
     }
 }
